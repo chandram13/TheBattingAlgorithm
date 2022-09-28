fa7e038745b58f758d5be5f500d14e0e9500b83b
Marvish Subhas Chandra <mschandr@uci.edu>
2022-09-27 19:44:19 -0700

    Add files via upload
    After considering the strike and foul ball occurrences, I determined that there is a number of given strikes and foul ball situations for each player.

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@
 
 // each hit represents 1 inning
 class baseball{
-    public static void teamOne(String[] n,s1,s2,s3,f1,f2,f3){
-        totalHits = ((1/9 * n) * 1/4 + s1 + f1) + ((1/9 * n) * 1/4 + s2 + f2) + ((1/9 * n) * 1/4 + s3 + f3) = 9
+    public static void teamOne(String[] n,s1,s2,s3,f1,f2,f3,x,y){
+        totalHits = ((1/9 * n) * 1/4 + (s1)^x + (f1)^y) + ((1/9 * n) * 1/4 + (s2)^x + (f2)^y) + ((1/9 * n) * 1/4 + (s3)^x + (f3)^y) = 9
         cout << "The total hits for the first baseball team is: " + cout << totalHits;
         return 0;
     }
-    public static void teamTwo(String[] n,s1,s2,s3,f1,f2,f3){
-        2nd_totalHits = ((1/9 * n) * 1/4 + s1 + f1) + ((1/9 * n) * 1/4 + s2 + f2) + ((1/9 * n) * 1/4 + s3 + f3) = 9
+    public static void teamTwo(String[] n,s1,s2,s3,f1,f2,f3,x,y){
+        2nd_totalHits = ((1/9 * n) * 1/4 + (s1)^x+ (f1)^y) + ((1/9 * n) * 1/4 + (s2)^x + (f2)^y) + ((1/9 * n) * 1/4 + (s3)^x + (f3)^y) = 9
         cout << "The total hits for the second baseball team is: " + cout << 2nd_totalHits;
         return 0;
     }
