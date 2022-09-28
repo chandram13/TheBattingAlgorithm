560cbad041be1359cc17ce272a09920ae971874f
Marvish Subhas Chandra <mschandr@uci.edu>
2022-09-27 19:14:45 -0700

    Add files via upload
    Updated algorithm to consider the strike and foul ball occurrences.

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@
 
 // each hit represents 1 inning
 class baseball{
-    public static void teamOne(String[] n){
-        totalHits = (1/9 * n) * 1/4 + (1/9 * n) * 1/4 + (1/9 * n) * 1/4 = 9;
+    public static void teamOne(String[] n,s1,s2,s3,f1,f2,f3){
+        totalHits = ((1/9 * n) * 1/4 + s1 + f1) + ((1/9 * n) * 1/4 + s2 + f2) + ((1/9 * n) * 1/4 + s3 + f3) = 9
         cout << "The total hits for the first baseball team is: " + cout << totalHits;
         return 0;
     }
-    public static void teamTwo(String[] n){
-        2nd_totalHits = (1/9 * n) * 1/4 + (1/9 * n) * 1/4 + (1/9 * n) * 1/4 = 9;
+    public static void teamTwo(String[] n,s1,s2,s3,f1,f2,f3){
+        2nd_totalHits = ((1/9 * n) * 1/4 + s1 + f1) + ((1/9 * n) * 1/4 + s2 + f2) + ((1/9 * n) * 1/4 + s3 + f3) = 9
         cout << "The total hits for the second baseball team is: " + cout << 2nd_totalHits;
         return 0;
     }
