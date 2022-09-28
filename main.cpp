cbc7beebbd9c7e318db22df0ee253480e0973027
Marvish Subhas Chandra <mschandr@uci.edu>
2022-09-27 18:31:26 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// Marvish Chandra
+// The Batting Algorithm
+
+#include <iostream>
+
+// each hit represents 1 inning
+class baseball{
+    public static void teamOne(String[] n){
+        totalHits = (1/9 * n) * 1/4 + (1/9 * n) * 1/4 + (1/9 * n) * 1/4 = 9;
+        cout << "The total hits for the first baseball team is: " + cout << totalHits;
+        return 0;
+    }
+    public static void teamTwo(String[] n){
+        2nd_totalHits = (1/9 * n) * 1/4 + (1/9 * n) * 1/4 + (1/9 * n) * 1/4 = 9;
+        cout << "The total hits for the second baseball team is: " + cout << 2nd_totalHits;
+        return 0;
+    }
+}
